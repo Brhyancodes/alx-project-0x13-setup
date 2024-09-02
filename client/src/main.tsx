b79bff99c6a5973bcbdb25e897e283239085d27c
Bereket Getachew <bakiget78@gmail.com>
2024-09-02 14:50:09 +0300

    install packages and change folder structure

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './pages/Home.tsx'
+import App from './pages/home/Home.tsx'
 import './index.css'
 import { createBrowserRouter } from 'react-router-dom'
 import { RouterProvider } from 'react-router-dom'
