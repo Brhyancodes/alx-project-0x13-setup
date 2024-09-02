b3287025f356f0afa6e363c5ee6821b4a7bf715c
osama seid <osaseid1@gmail.com>
2024-09-02 08:26:12 +0300

    authentiaction pages are done! and we should style according to design

---
+++ client/src/pages/auth/forgetpw.tsx
@@ -0,0 +1,161 @@
+import React, { useState } from 'react';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import myImage from '../../assets/bg-auth-2.png';
+import bgImage from '../../assets/authoBg.png';
+
+
+// password recovery form using Zod
+const passwordRecoverySchema = z.object({
+  email: z.string().email('Please enter a valid email address'),
+});
+
+type PasswordRecoveryFormData = z.infer<typeof passwordRecoverySchema>;
+
+const PasswordRecoveryForm: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+  const [timer, setTimer] = useState(120);
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PasswordRecoveryFormData>();
+
+  const onSubmit = async (_data: PasswordRecoveryFormData) => {
+    try {
+      setLoading(true);
+      setError('');
+
+      // password recovery logic here
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      setSuccess(true);
+
+      const interval = setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
+
+      setTimeout(() => {
+        clearInterval(interval);
+        navigate('/login');
+      }, 120000);
+    } catch (err) {
+      setError('An error occurred. Please try again later.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleForgotPassword = () => {
+    navigate('/login');
+  };
+  const handleNewAccount = () => {
+    navigate('/register');
+  };
+
+  return (
+    <>
+      <div
+        className="flex justify-center items-center w-screen h-screen "
+        style={{
+          backgroundImage: `url(${bgImage})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        <div className="bg-gray-50 rounded-lg pl-2  pr-6 mb-4 w-full max-w-3xl  flex flex-col md:flex-row sm:flex-row">
+      <div className="md:w-1/2  sm:w-1/3 bg-cover bg-center hidden md:block sm:block mr-8 rounded-lg py-4 ml-none mb-2  mt-2 pt-4 pb-4"
+        style={{ backgroundImage: `url(${myImage })` }}>
+      </div>
+
+          <div className="flex-1 ">
+          <div className="flex-1 flex justify-between items-center ">
+              <h2 className="font-bold mb-4 ">Forget Password</h2>
+              <h2 className="font-bold mb-4 ">filix<span className='text-blue-500'>pharma</span></h2>
+            </div>
+
+            {success ? (
+              <div className="text-green-500 mb-4">
+                Password reset instructions have been sent to your email. You will
+                receive an Email in {timer} seconds.
+              </div>
+            ) : (
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="mb-4 ">
+                  <label htmlFor="email" className="block font-medium mb-2 ">
+                    Email Address
+                  </label>
+                  <input
+                    placeholder="Enter Your Email"
+                    id="email"
+                    type="email"
+                    className={`border rounded-md py-2 px-3 w-full ${
+                      errors.email ? 'border-red-500' : 'border-gray-300'
+                    }`}
+                    {...register('email', {
+                      required: 'Email is required',
+                      pattern: {
+                        value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                        message: 'Please enter a valid email address',
+                      },
+                    })}
+                  />
+                  {errors.email && (
+                    <p className="text-red-500 mt-2">{errors.email.message}</p>
+                  )}
+                </div>
+
+                <button
+                  type="submit"
+                  className={`bg-blue-500 hover:bg-green-600 text-white font-medium py-2 px-4 w-full rounded-md transition-colors  ${
+                    loading ? 'cursor-not-allowed opacity-50' : ''
+                  }`}
+                  disabled={loading}
+                >
+                  {loading ? 'Loading...' : 'Send Reset Link'}
+                </button>
+
+                {error && (
+                  <div className="text-red-500 mt-4">{error}</div>
+                )}
+                <div className="flex mt-3">
+                  <hr className="w-1/2 mt-2 border-dashed"></hr>
+                  <p className="text-center">Or</p>
+                  <hr className="w-1/2 mt-2 border-dashed"></hr>
+                </div>
+
+                <div className="mt-2 text-center flex">
+                  <p className="text-left">I Remembered The Password? </p>
+                  <button
+                    type="button"
+                    className="text-blue-500 hover:text-green-600 font-medium ml-2"
+                    onClick={handleForgotPassword}
+                  >
+                    login
+                  </button>
+                </div>
+                <div className="mt-4 mb-2 text-center flex">
+                  <p className="text-center">New Account?</p>
+                  <button
+                    type="button"
+                    className="text-blue-500 hover:text-green-600 font-medium ml-2 "
+                    onClick={handleNewAccount}
+                  >
+                    Register Now
+                  </button>
+                </div>
+              </form>
+            )}
+          </div>
+          
+        </div>
+      </div>
+          </>
+  );
+};
+
+export default PasswordRecoveryForm;
---
+++ client/src/pages/auth/login.tsx
@@ -0,0 +1,106 @@
+import { Link, useNavigate } from 'react-router-dom';
+import backgroundImage from '../../assets/debbal-bg.jpg';
+import photo from '../../assets/debbal-logo.jpg';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { FieldError } from 'react-hook-form';
+
+const loginSchema = z.object({
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(8, 'Password must be at least 8 characters'),
+});
+
+type LoginFormData = z.infer<typeof loginSchema>;
+
+const LoginPage = () => {
+  const navigate = useNavigate();
+  const { register, handleSubmit, formState: { errors } } = useForm<LoginFormData>({
+    // resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit = async (data: LoginFormData) => {
+    navigate('/dashboard');
+  };
+
+  return (
+    <div
+      className="flex justify-center items-center h-screen bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundColor: 'rgba(4, 1, 4, 1)',
+      }}
+    >
+      <div className="bg-gray-50 rounded-xl w-full max-w-xl p-2 flex flex-col lg:flex-row mx-4 sm:mx-0">
+        <div
+          className="lg:w-1/2 bg-cover bg-center hidden lg:block mr-8 rounded-xl ml-none mb-2 mt-2 pt-4 pb-4"
+          style={{ backgroundImage: `url(${photo})` }}
+        ></div>
+        <div className="lg:w-1/2 pt-6 pb-8 px-4 sm:px-6 md:px-0">
+          <div className="flex justify-between items-center">
+            <h1 className="text-xl font-semibold text-gray-700 mb-4">Login</h1>
+            <h1 className="text-right font-bold text-blue-500 sm:text-xl">filix-pharma</h1>
+          </div>
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-base font-semibold text-gray-700 mb-2">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+                {...register('email')}
+                className={`border rounded w-full py-2 px-3 leading-tight focus:outline-none ${
+                  errors.email ? 'border-red-500' : ''
+                }`}
+              />
+              {errors.email && (
+                <p className="text-red-500">{(errors.email as FieldError).message}</p>
+              )}
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="password" className="block text-base font-semibold text-gray-700 mb-2">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+                {...register('password')}
+                className={`border rounded w-full py-2 px-3 leading-tight focus:outline-none ${
+                  errors.password ? 'border-red-500' : ''
+                }`}
+              />
+              {errors.password && (
+                <p className="text-red-500">{(errors.password as FieldError).message}</p>
+              )}
+            </div>
+
+            <button
+              type="submit"
+              className="mb-2 w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none"
+            >
+              Login
+            </button>
+          </form>
+          <div className="flex items-center my-4">
+            <div className="bg-gray-400 flex-grow h-px"></div>
+            <span className="px-4 font-semibold text-black-500">Or</span>
+            <div className="bg-gray-400 flex-grow h-px"></div>
+          </div>
+
+          <p className="mt-2 text-center">
+            Don't have an account?{' '}
+            <Link to="/register" className="text-blue-500 font-bold hover:text-blue-700">
+              Register
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
