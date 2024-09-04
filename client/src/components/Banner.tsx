b80b4a0be6cd2c32f5e42d2f48691a3b1f24a70e
Bereket Getachew <bakiget78@gmail.com>
2024-09-04 20:31:25 +0300

    85% of the home page is done

---
+++ client/src/components/Banner.tsx
@@ -0,0 +1,71 @@
+import { XMarkIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Example: React.FC = () => {
+  const [show, setShow] = useState(true);
+
+  return (
+    <>
+      {show && (
+        <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-green-500 px-6 py-1.5 sm:px-3.5 sm:before:flex-1">
+          <div
+            aria-hidden="true"
+            className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+          >
+            <div
+              style={{
+                clipPath:
+                  "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
+              }}
+              className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#7e5767] to-[#535080] opacity-30"
+            />
+          </div>
+          <div
+            aria-hidden="true"
+            className="absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+          >
+            <div
+              style={{
+                clipPath:
+                  "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
+              }}
+              className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#845e6e] to-[#484477] opacity-30"
+            />
+          </div>
+          <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
+            <p className="text-sm leading-6 text-gray-900">
+              <strong className="font-semibold">Felix Pharmacy</strong>
+              <svg
+                viewBox="0 0 2 2"
+                aria-hidden="true"
+                className="mx-2 inline h-0.5 w-0.5 fill-current"
+              >
+                <circle r={1} cx={1} cy={1} />
+              </svg>
+              Join us from June 7 – 9 to get a 15% discount.
+            </p>
+            <Link
+              to="/login"
+              className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+            >
+              Register now <span aria-hidden="true">&rarr;</span>
+            </Link>
+          </div>
+          <div className="flex flex-1 justify-end">
+            <button
+              type="button"
+              className="-m-3 p-3 focus-visible:outline-offset-[-4px]"
+              onClick={() => setShow(false)}
+            >
+              <span className="sr-only">Dismiss</span>
+              <XMarkIcon aria-hidden="true" className="h-5 w-5 text-gray-900" />
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Example;
